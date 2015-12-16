4c87e242f6103736f6bdfab615653fff26ea59aa
kazz4423 <kazuki183@gmail.com>
2015-12-16 18:30:37 +0900

    ファーストコミット

---
+++ TemplateMatchingOpenCVShape/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace TemplateMatchingOpenCVShape
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxIpl1 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBoxIpl2 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxIpl1
+            // 
+            this.pictureBoxIpl1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxIpl1.Name = "pictureBoxIpl1";
+            this.pictureBoxIpl1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBoxIpl1.TabIndex = 0;
+            this.pictureBoxIpl1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(531, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "テンプレート画像";
+            // 
+            // pictureBoxIpl2
+            // 
+            this.pictureBoxIpl2.Location = new System.Drawing.Point(535, 46);
+            this.pictureBoxIpl2.Name = "pictureBoxIpl2";
+            this.pictureBoxIpl2.Size = new System.Drawing.Size(32, 32);
+            this.pictureBoxIpl2.TabIndex = 2;
+            this.pictureBoxIpl2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(534, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 56);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "マッチング開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(532, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "相関係数";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(643, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "CV_TM_SQDIFF",
+            "CV_TM_SQDIFF_NORMED",
+            "CV_TM_CCORR",
+            "CV_TM_CCORR_NORMED",
+            "CV_TM_CCOEFF",
+            "CV_TM_CCOEFF_NORMED"});
+            this.comboBox1.Location = new System.Drawing.Point(534, 112);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(167, 26);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "CV_TM_SQDIFF";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(725, 512);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBoxIpl2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxIpl1);
+            this.Name = "Form1";
+            this.Text = "テンプレートマッチングテスト";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl1;
+        private System.Windows.Forms.Label label1;
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ TemplateMatchingOpenCVShape/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+
+namespace TemplateMatchingOpenCVShape
+{
+    public partial class Form1 : Form
+    {
+
+        private Mat srcImg;
+        private Mat tmpImg;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //画像ファイルの読み込み
+            srcImg = new Mat("../../lena.jpg");
+            pictureBoxIpl1.ImageIpl = srcImg.ToIplImage();
+
+            //テンプレート画像の読み込み
+            tmpImg = new Mat("../../lena_tmp.jpg");
+            pictureBoxIpl2.ImageIpl = tmpImg.ToIplImage();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MatchTemplateMethod mtm;
+            bool mtmFlag;
+
+            getMatchTmplateMethod(out mtm, out mtmFlag);
+
+            CvMat result = new CvMat(srcImg.Height - tmpImg.Height + 1, srcImg.Width - tmpImg.Width + 1, MatrixType.F32C1); // リザルト
+            Cv.MatchTemplate(srcImg.ToCvMat(), tmpImg.ToCvMat(), result, mtm);
+            CvPoint minPoint = new CvPoint();
+            CvPoint maxPoint = new CvPoint();
+            Cv.MinMaxLoc(result, out minPoint, out maxPoint);
+            
+            IplImage resultImg = srcImg.ToIplImage(); // picturBoxIplには直接Rectを書き込めないのでバッファを挟む
+            if (mtmFlag)
+            {
+                CvRect rect = new CvRect(maxPoint, tmpImg.ToCvMat().GetSize());
+                resultImg.DrawRect(rect, new CvScalar(0, 255, 0), 2);
+                label3.Text = result[maxPoint.X, maxPoint.Y].ToString(); // 相関係数の取得
+            }
+            else
+            {
+                CvRect rect = new CvRect(minPoint, tmpImg.ToCvMat().GetSize());
+                resultImg.DrawRect(rect, new CvScalar(0, 255, 0), 2);
+                label3.Text = result[minPoint.X, minPoint.Y].ToString(); // 相関係数の取得
+            }
+            
+            pictureBoxIpl1.ImageIpl = resultImg;
+
+            
+        }
+
+        public void getMatchTmplateMethod(out MatchTemplateMethod _mtm, out bool flag)
+        {
+            switch (comboBox1.Text)
+            {
+                case "CV_TM_SQDIFF":
+                    _mtm = MatchTemplateMethod.SqDiff;
+                    flag = false;
+                    break;
+                case "CV_TM_SQDIFF_NORMED":
+                    _mtm = MatchTemplateMethod.SqDiffNormed;
+                    flag = false;
+                    break;
+                case "CV_TM_CCORR":
+                    _mtm = MatchTemplateMethod.CCorr;
+                    flag = true;
+                    break;
+                case "CV_TM_CCORR_NORMED":
+                    _mtm = MatchTemplateMethod.CCorrNormed;
+                    flag = true;
+                    break;
+                case "CV_TM_CCOEFF":
+                    _mtm = MatchTemplateMethod.CCoeff;
+                    flag = true;
+                    break;
+                case "CV_TM_CCOEFF_NORMED":
+                    _mtm = MatchTemplateMethod.CCoeffNormed;
+                    flag = true;
+                    break;
+                default:
+                    _mtm = MatchTemplateMethod.SqDiff;
+                    flag = false;
+                    break;
+            }
+        }
+    }
+}
